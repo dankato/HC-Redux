d2d5a6a10589cb0b3d83d7804bb39b9c10c5517f
Lewi Gilamichael <dr.lewi@gmail.com>
2017-07-31 15:45:11 -0400

    restructured files

---
+++ src/reducer.js
@@ -0,0 +1,24 @@
+import * as actions from './actions';
+
+const initialState = {
+    guesses, 
+    feedback,
+    correctAnswer: Math.floor(Math.random() * 100) + 1
+};
+
+export const newGameReducer = (state = initialState, action) => {
+    if (action.type === actions.newGame) {
+        console.log('It was a new game');
+        return Object.assign({}, state, {
+          guesses: [],
+          feedback: 'Make your guess!',
+          correctAnswer: Math.floor(Math.random() * 100) + 1,  
+        });
+    } else if (action.type === actions.DECREMENT) {
+        console.log('It was an decrement');
+        return Object.assign({}, state, {
+            counter: state.counter - action.by
+        });
+    }
+    return state;
+};
