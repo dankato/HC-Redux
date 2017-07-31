fe9d9fca216c432d841d8d4e44bbfb60a9d56acd
dankato <apearmor@gmail.com>
2017-07-31 12:19:08 -0700

    adding init files, created actions

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+
+export const NEWGAME = 'NEWGAME';
+export const newGame = ({
+    type: NEWGAME,
+});
+
+export const TOGGLE_INFO = 'TOGGLE_INFO';
+export const toggleInfo = ({
+    type: TOGGLE_INFO,
+});
+
+export const GUESS = "GUESS";
+export const guess = ({
+    type: GUESS,
+});
